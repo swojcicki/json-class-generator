15adf6fe46a26e3be440195f850542ac48faba51
swojcicki <slawek23>
2015-06-19 21:41:41 +0200

    some refactoring, version 0.1

--- run-example.sh
+++ run-example.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 mvn clean compile exec:java -Dexec.mainClass="pl.edu.agh.student.wojcicks.JsonReader"
--- src/main/java/pl/edu/agh/student/wojcicks/ClassRepresentation.java
+++ src/main/java/pl/edu/agh/student/wojcicks/ClassRepresentation.java
@@ -33,12 +33,15 @@ public class ClassRepresentation {
       sb.append(NEW_LINE);
     }
     boolean hasList = false;
+    String fieldsSt = "";
+    String setGetSt = "";
+
     Set<String> keys = fields.keySet();
     for (String key : keys) {
-      if (fields.get(key).contains("List")) {
-        hasList = true;
-        break;
-      }
+      hasList = checkHasList(hasList, key);
+
+      fieldsSt = generateFields(key);
+      setGetSt = generateGetSet(key);
     }
     if (fields.containsValue("List") || hasList) {
       sb.append("import ").append("java.util.List").append(";").append(NEW_LINE);
@@ -47,28 +50,42 @@ public class ClassRepresentation {
     }
     sb.append("public class ").append(className).append(" {").append(NEW_LINE);
     sb.append(NEW_LINE);
-
-    for (String key : keys) {
-      if (fields.get(key).startsWith("List")) {
-        sb.append(IND).append("private ").append(fields.get(key)).append(" ").append(key).append(" = new Array").append(fields.get(key)).append("()").append(";").append(NEW_LINE);
-      } else {
-        sb.append(IND).append("private ").append(fields.get(key)).append(" ").append(key).append(";").append(NEW_LINE);
-      }
-    }
+    sb.append(fieldsSt);
     sb.append(NEW_LINE);
-    for (String key : keys) {
-      sb.append(IND).append("public ").append(fields.get(key)).append(" get").append(StringUtils.firstUpperCase(key)).append("() {").append(NEW_LINE);
-      sb.append(IND).append(IND).append("return ").append(key).append(";").append(NEW_LINE);
-      sb.append(IND).append("}").append(NEW_LINE);
-      sb.append(NEW_LINE);
-      sb.append(IND).append("public void set").append(StringUtils.firstUpperCase(key)).append("(").append(fields.get(key)).append(" ").append(key).append(") {").append(NEW_LINE);
-      sb.append(IND).append(IND).append("this.").append(key).append(" = ").append(key).append(";").append(NEW_LINE);
-      sb.append(IND).append("}").append(NEW_LINE);
-      sb.append(NEW_LINE);
-    }
+    sb.append(setGetSt);
     sb.append("}");
 
     return sb.toString();
+  }
+
+  private String generateGetSet(String key) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(IND).append("public ").append(fields.get(key)).append(" get").append(StringUtils.firstUpperCase(key)).append("() {").append(NEW_LINE);
+    sb.append(IND).append(IND).append("return ").append(key).append(";").append(NEW_LINE);
+    sb.append(IND).append("}").append(NEW_LINE);
+    sb.append(NEW_LINE);
+    sb.append(IND).append("public void set").append(StringUtils.firstUpperCase(key)).append("(").append(fields.get(key)).append(" ").append(key).append(") {").append(NEW_LINE);
+    sb.append(IND).append(IND).append("this.").append(key).append(" = ").append(key).append(";").append(NEW_LINE);
+    sb.append(IND).append("}").append(NEW_LINE);
+    sb.append(NEW_LINE);
+    return sb.toString();
+  }
+
+  private String generateFields(String key) {
+    StringBuilder sb = new StringBuilder();
+    if (fields.get(key).startsWith("List")) {
+      sb.append(IND).append("private ").append(fields.get(key)).append(" ").append(key).append(" = new Array").append(fields.get(key)).append("()").append(";").append(NEW_LINE);
+    } else {
+      sb.append(IND).append("private ").append(fields.get(key)).append(" ").append(key).append(";").append(NEW_LINE);
+    }
+    return sb.toString();
+  }
+
+  private boolean checkHasList(boolean hasList, String key) {
+    if (!hasList && fields.get(key).contains("List")) {
+      hasList = true;
+    }
+    return hasList;
   }
 
   public void setPackageName(String packageName) {
